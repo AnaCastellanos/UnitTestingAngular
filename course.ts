d620d3be536ff66e0bce81cf46ddfe329a7fda46
Ana Castellanos <ana.lec15@gmail.com>
2022-10-20 21:09:32 -0500

    Add files via upload

---
+++ course.ts
@@ -0,0 +1,6 @@
+export interface Course {
+  id: number;
+  title: string;
+  category: string;
+  lessonsCount: number;
+}
---
+++ courses.ts
@@ -0,0 +1,94 @@
+import { Course } from "../models/course";
+
+export const COURSES: {[key: number]: Course} = {
+  15: {
+    id: 15,
+    title: 'Angular Testing Course',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  14: {
+    id: 14,
+    title: 'Angular Testing Course',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  13: {
+    id: 13,
+    title: 'Angular Testing Course',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  12: {
+    id: 12,
+    title: 'Angular Testing Course',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  11: {
+    id: 11,
+    title: 'Angular Core Deep Dive',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  10: {
+    id: 10,
+    title: 'Angular Testing Course',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  9: {
+    id: 9,
+    title: 'RXJS Beginners',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  8: {
+    id: 8,
+    title: 'RXJS Medium',
+    category: 'Medium',
+    lessonsCount: 10
+  },
+  7: {
+    id: 7,
+    title: 'RXJS Advance',
+    category: 'Advance',
+    lessonsCount: 10
+  },
+  6: {
+    id: 6,
+    title: 'Angular Jasmine and Karma',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  5: {
+    id: 5,
+    title: 'Angular Jasmine Course',
+    category: 'Medium',
+    lessonsCount: 10
+  },
+  4: {
+    id: 4,
+    title: 'Angular Routing Course',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  3: {
+    id: 3,
+    title: 'Angular Interfaces Course',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  2: {
+    id: 2,
+    title: 'Angular Mocks Course',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+  1: {
+    id: 1,
+    title: 'Angular Data Course',
+    category: 'Beginner',
+    lessonsCount: 10
+  },
+}
